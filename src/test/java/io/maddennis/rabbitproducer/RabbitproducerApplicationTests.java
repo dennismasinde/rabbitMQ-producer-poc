1cdbba126bb5465e9fe6dc87a94f2425fcbf48ec
dennismasinde <chengwadennismasinde@gmail.com>
2023-05-10 18:12:45 +0300

    rabbitMQ producer poc

---
+++ src/test/java/io/maddennis/rabbitproducer/RabbitproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maddennis.rabbitproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
